9b1857606fde0f16eb820ed7e4b3dbb467878e00
Drew1998-ocan <drewacademics@gmail.com>
2022-09-09 21:15:02 -0700

    printing a string using the puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio>
+/*
+ * entry point to the  main
+ * returns a string: ""Programming is like building a multilingual puzzle,"
+ * */
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+}
