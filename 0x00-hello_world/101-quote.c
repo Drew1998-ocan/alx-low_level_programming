96ebcb4323a47ca4f9f0f9350a528339e9fbf119
Drew1998-ocan <drewacademics@gmail.com>
2022-09-09 22:50:34 -0700

    new using the write function to output

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,21 @@
 #include <unistd.h>
 
-/** 
- * main - prints the standrad error sandwiched around the  standard output
+/**
+ * main - Entry point
  *
- * Return: returns 1
+ * Description: 'the program's description'
  *
+ * Return: will be 1 on success
  **/
+
+
 
 int main(void)
 
 {
 
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
-	return (1);
+			return (1);
 
 }
