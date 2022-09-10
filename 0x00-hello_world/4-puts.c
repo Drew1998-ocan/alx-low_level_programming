7f636d577da4e7c50df424dd06770af2a4c175ce
Drew1998-ocan <drewacademics@gmail.com>
2022-09-09 21:26:47 -0700

    printing the atring using the puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,18 @@
-#include <stdio>
-/*
- * entry point to the  main
- * returns a string: ""Programming is like building a multilingual puzzle,"
- * */
-int main (void)
+#include <stdio.h>
+
+/**
+ * main - main block
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+
 {
+
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+
+	return (0);
+
 }
