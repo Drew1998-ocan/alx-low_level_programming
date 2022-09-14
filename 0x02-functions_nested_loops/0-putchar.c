49b73121d599a723e0cf0bb53101b7019391971b
Drew1998-ocan <drewacademics@gmail.com>
2022-09-14 16:34:22 -0700

    printing the character _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,43 @@
 #include "main.h"
-/**
- * main - Entry point
- *
- * Return: Always 0  on success
- **/
-int main(void)
-{
-	char ch[] = "_putchar";
-	int i;
 
-	for (i = 0; i <= 8; i++)
-	{
-		_putchar(ch[i]);
-	}
+
+
+/**
+
+ * main - print putchar
+
+ * Return: always 0
+
+ */
+
+
+
+int main(void)
+
+{
+
+	_putchar('_');
+
+	_putchar('p');
+
+	_putchar('u');
+
+	_putchar('t');
+
+	_putchar('c');
+
+	_putchar('h');
+
+	_putchar('a');
+
+	_putchar('r');
+
 	_putchar('\n');
+
+
+
 	return (0);
+
+
+
 }
