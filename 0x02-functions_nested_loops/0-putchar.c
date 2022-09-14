e00941185c7ac2e6c999e6fea1bbd4a1ff3bb4b1
Drew1998-ocan <drewacademics@gmail.com>
2022-09-14 01:08:22 -0700

    print putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,10 +9,10 @@ int main(void)
 	char ch[] = "_putchar";
 	int i;
 
-	for (i = 0; i <= 8; i++ )
+	for (i = 0; i <= 8; i++)
 	{
-		putchar(ch[i]);
+		_putchar(ch[i]);
 	}
-	putchar('\n');
+	_putchar('\n');
 	return (0);
 }
