b628d882a1d16dcc7bc645c61b18a22bf8eb5365
Drew1998-ocan <drewacademics@gmail.com>
2022-09-14 00:34:14 -0700

    printing _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0  on success
+ **/
+int main(void)
+{
+	putchar("_putschar");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+this directory contains nested loops and functions, good luck to me
