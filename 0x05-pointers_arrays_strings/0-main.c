8dfd50b3b7f6008e2a78bab840ed97f9e3f422cf
Drew1998-ocan <drewacademics@gmail.com>
2022-09-19 15:25:20 -0700

    ointer one

--- 0x05-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -1,20 +1,17 @@
 #include "main.h"
 /**
- * main - checks the code 
+ * main - checks the code
  *
  * Return:  Always 0 (on success)
- * 
+ *
  **/
 int main(void)
 {
 	int n;
 
 	n = 402;
-
 	printf("n = %d\n", n);
-
 	reset_to_98(&n);
-
 	printf("n = %d\n", n);
 	return (0);
 }
--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 /**
- * void reset_to_98 - function for updatin the value 
- *
+ * function void reset_to_98 - function for updatin the value
+ * @n is the variable
  * Return : 0 (on success)
  *
  **/
 void reset_to_98(int *n)
 {
-	
 	*n = 98;
-	
 }
